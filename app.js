61dd5a7acf68122b4853e5d33d2b531b016d436b
Al-Fareed <alfareedss472@gmail.com>
2023-04-20 17:08:19 +0530

    Done with this project
    Successfully works

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// * we can add and get the products in two ways using mongoose or without using mongoose
+// below code has implemented mongoose, we can also implement without using mongoose
+// change line no.6 [ const mongoPractice = require('./mongoose'); ] => [ const mongoPractice = require('./mongo'); ]
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoPractice = require('./mongoose');
